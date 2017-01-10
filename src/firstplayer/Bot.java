a1a16c543412557f3ced57b0348602a9ed3e2ef3
Lizhou Sha <slz@mit.edu>
2017-01-10 16:08:45 -0500

    Add more global variables; mechanism to update globals

--- src/firstplayer/Bot.java
+++ src/firstplayer/Bot.java
@@ -4,11 +4,17 @@ import battlecode.common.*;
 
 public strictfp class Bot {
   public static RobotController rc;
+  public static RobotType myType;
+  public static Team us;
+  public static Team them;
+  
+  public static MapLocation here;
   
   public static void loop(RobotController rc_) {
     init(rc_);
     while (true) {
       try {
+        update();
         doTurn();
       } catch (GameActionException e) {
         System.out.printf("Bot %d (%s) threw %s", rc.getID(), rc.getType().name(), e.toString());
@@ -19,6 +25,15 @@ public strictfp class Bot {
   
   public static void init(RobotController rc_) {
     rc = rc_;
+    myType = rc.getType();
+    us = rc.getTeam();
+    them = us.opponent();
+    
+    here = rc.getLocation();
+  }
+  
+  public static void update() {
+    here = rc.getLocation();
   }
   
   public static void doTurn() throws GameActionException {
