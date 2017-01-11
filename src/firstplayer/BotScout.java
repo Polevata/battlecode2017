1261cafe561557fae763c3bb33b3e884c1b231b0
Lizhou Sha <slz@mit.edu>
2017-01-10 22:52:06 -0500

    Scouts now shake trees and harrass enemy

--- src/firstplayer/BotScout.java
+++ src/firstplayer/BotScout.java
@@ -26,14 +26,37 @@ public strictfp class BotScout extends Bot {
 
   public static void doTurn() throws GameActionException {
     explore();
+    tryShake();
     harrass();
+  }
+  
+  public static void tryShake() throws GameActionException {
+    TreeInfo[] neutralTrees = rc.senseNearbyTrees(myType.strideRadius, Team.NEUTRAL);
+    int treeId;
+    for (int i = 0; i < neutralTrees.length; i++) {
+      treeId = neutralTrees[i].ID;
+      if (rc.canShake(treeId)) {
+        System.out.printf("Shaking tree %d", treeId);
+        rc.shake(treeId);
+      }
+    }
   }
 
   static void explore() throws GameActionException {
     tryMove(randomDirection());
   }
 
-  static void harrass() {
+  static void harrass() throws GameActionException {
+    RobotInfo[] robots = rc.senseNearbyRobots(-1, them);
+
+    // If there are some...
+    if (robots.length > 0) {
+      // And we have enough bullets, and haven't attacked yet this turn...
+      if (rc.canFireSingleShot()) {
+        // ...Then fire a bullet in the direction of the enemy.
+        rc.fireSingleShot(rc.getLocation().directionTo(robots[0].location));
+      }
+    }
   }
 
 }
